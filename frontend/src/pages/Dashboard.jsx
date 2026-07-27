13a3929c5be0a1f4ceaecebbb6564ff22ce87311
HassanAhmed270 <hassaanuser@gmail.com>
2026-07-27 18:02:45 +0500

    Revert "Dashboard UI"
    This reverts commit 0724f2cb4c1abc737a60ccca639d77a79ca3ae45.

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,85 +1,11 @@
-import { useState,useEffect } from "react";
-import Heading from "../components/Heading";
-import Sidebar from "../components/Sidebar";
-import Notepad from "../components/Notepad";
-import AvailableNotes from "../components/AvailableNotes";
+import React from 'react'
 
 const Dashboard = () => {
-  const [isOpen, setIsOpen] = useState(true);
-
-  const toggleSidebar = () => setIsOpen(!isOpen);
-  useEffect(() => {
-  const handleResize = () => {
-    if (window.innerWidth >= 768) {
-      setIsOpen(true);
-    }
-  };
-
-  // Set correct state on initial load
-  handleResize();
-
-  window.addEventListener("resize", handleResize);
-
-  return () => window.removeEventListener("resize", handleResize);
-}, []);
-
-  const handleNew = () => console.log("New clicked");
-  const handleSave = () => console.log("Save clicked");
-
   return (
-    <div className="min-h-screen bg-gray-50">
-      {/* Header */}
-      <div className="fixed top-0 left-0 w-full h-16 z-50">
-        <Heading
-          isOpen={isOpen}
-          toggleSidebar={toggleSidebar}
-        />
-      </div>
-
-      {/* Overlay */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black/50 z-30 md:hidden"
-          onClick={toggleSidebar}
-        />
-      )}
-
-      {/* Sidebar */}
-      <div
-        className={`
-          fixed
-          top-16
-          left-0
-          w-64
-          h-[calc(100vh-4rem)]
-          bg-gray-800
-          transition-transform
-          duration-300
-          z-40
-          ${isOpen ? "translate-x-0" : "-translate-x-full"}
-        `}
-      >
-        <Sidebar
-          onNew={handleNew}
-          onSave={handleSave}
-        />
-      </div>
-
-      {/* Main Content */}
-      <main
-        className={`
-          pt-16
-          transition-all
-          duration-300
-          ${isOpen ? "md:ml-64" : "md:ml-0"}
-        `}
-      >
-        <div className="flex flex-col min-h-[calc(100vh-4rem)]">
-          <Notepad />
-        </div>
-      </main>
+    <div>
+      Dashbaoard
     </div>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
